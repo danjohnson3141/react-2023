49431920728f3ae3af5e442d012fa3060ed13c0d
Dan Johnson <Dan.Johnson@gartner.com>
2023-04-17 14:58:35 -0700

    rmc comment

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,6 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2023, 1, 22)
-  // const expenseDescription = 'Tools'
-  // const expensePrice = 3141.59
   return (
     <div className='expense-item'>
       <div><p>{props.date?.toISOString()}</p></div>
