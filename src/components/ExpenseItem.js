f07f2c000e72dccd2bb4f833ee7d2bdceecf21dc
Dan Johnson <Dan.Johnson@gartner.com>
2023-04-17 14:49:23 -0700

    test

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 1, 22)
-  const expenseDescription = 'Tools'
-  const expensePrice = 3141.59
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2023, 1, 22)
+  // const expenseDescription = 'Tools'
+  // const expensePrice = 3141.59
   return (
     <div className='expense-item'>
-      <div><p>{expenseDate.toISOString()}</p></div>
+      <div><p>{props.date?.toISOString()}</p></div>
       <div className='expense-item__description'>
-        <h2>{expenseDescription}</h2>
-        <div className='expense-item__price'>{expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
       </div>
     </div>
   )
