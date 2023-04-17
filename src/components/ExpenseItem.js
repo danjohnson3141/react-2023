5220cd8916c9d42a41a9bbb6b61890271940faf5
Dan Johnson <Dan.Johnson@gartner.com>
2023-04-17 14:58:26 -0700

    formatting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,7 +9,7 @@ function ExpenseItem(props) {
       <div><p>{props.date?.toISOString()}</p></div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   )
