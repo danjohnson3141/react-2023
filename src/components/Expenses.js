dc8f3d9a3a699aaafb4a594191b89e96976df1f1
Dan Johnson <Dan.Johnson@gartner.com>
2023-04-18 07:55:39 -0700

    try .map

---
+++ src/components/Expenses.js
@@ -0,0 +1,22 @@
+import ExpenseItem from "./ExpenseItem"
+import "./Expenses.css"
+
+function Expenses(props) {
+  console.log('hi', props)
+  if (props !== null) {
+    props?.map(expense => {
+      return (
+        <ExpenseItem
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      )
+    })
+  } else {
+    <p>No expenses today</p>
+  }
+}
+
+export default Expenses
